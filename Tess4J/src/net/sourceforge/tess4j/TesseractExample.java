9660475b9c46955a63883da262f52934c0a2a407
isaac <isaacjs2@illinois.edu>
2020-03-09 16:49:47 -0500

    Works for one image!

--- Tess4J/src/net/sourceforge/tess4j/TesseractExample.java
+++ Tess4J/src/net/sourceforge/tess4j/TesseractExample.java
@@ -4,8 +4,10 @@ import java.io.File;
 
 public class TesseractExample {
     public static void main(String[] args) {
-        File imageFile = new File("ocrTest.png");
+        File imageFile = new File("C:\\Users\\isaac\\IdeaProjects\\CSForGood-Sp2020\\Tess4J\\test\\resources\\test-data\\eurotext_unlv.png");
         Tesseract instance = new Tesseract();
+        instance.setDatapath("C:\\Users\\isaac\\IdeaProjects\\CSForGood-Sp2020\\Tess4J\\tessdata");
+        instance.setLanguage("eng");
 
         try {
             String result = instance.doOCR(imageFile);
