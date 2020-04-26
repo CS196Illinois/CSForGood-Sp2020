af1657d4975a789c2af0fec37f8b0ea6c2d77d97
Madison <mcy2@illinois.edu>
2020-04-26 13:11:17 -0500

    fixed issue from last commit

--- front-end-react/App.js
+++ front-end-react/App.js
@@ -1,85 +1,13 @@
 import React from 'react';
 
-import {
-  StyleSheet,
-  Text,
-  View,
-  Button,
-  Separator,
-  SafeAreaView,
-  color,
-  accessibilityLabel,
-} from 'react-native';
-import Constants from 'expo-constants';
+import CameraPage from './src/camera.page';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <View style={styles.title}>
-        <Text>Welcome to our app (that we havent named yet)</Text>
-      </View>
-      <View style={styles.bottom}>
-        <View style={styles.button}>
-          <Button
-            onPress={() => {
-              alert('This would open up the camera');
-              respond();
-            }}
-            color="#841584"
-            title="Camera"
-          />
-        </View>
-        <View style={styles.button}>
-          <Button
-            onPress={() => {
-              alert('This would open up the camera roll');
-              respond();
-            }}
-            color="#123456"
-            title="Camera Roll"
-          />
-        </View>
-        <View style={styles.button}>
-          <Button
-            onPress={() => {
-              alert('This would open up the history / ask for login information');
-              respond();
-            }}
-            color="#654321"
-            title="History"
-          />
-        </View>
-      </View>
-    </View>
-  );
-}
+import { StyleSheet, Text, View } from 'react-native';
 
-export function respond() {
-  return <text>Response</text>;
-  //this function when called will just say respons the export default means we can use it throughout the app
-  // later use <text>good job on the {input char here} letter
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  button: {
-    flex: 1,
-    backgroundColor: '#fff',
-    justifyContent: 'space-between',
-  },
-  title: {
-    flex: 9,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  bottom: {
-    flex: 1,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-  }
-});
+export default class App extends React.Component {
+  render() {
+    return (
+      <CameraPage />
+    );
+  };
+};
