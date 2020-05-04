2168a41057961d9a8d3d1a95c1d57524f7372ca3
gprakarsh <379@doonschool.com>
2020-05-04 11:08:43 +0530

    worked on backend integration...issue with file upload still there

--- front-end-react/src/result.page.js
+++ front-end-react/src/result.page.js
@@ -2,29 +2,63 @@ import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, Image, TextInput } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import axios from 'axios';
+import * as ImageManipulator from "expo-image-manipulator";
+import { Asset } from "expo-asset";
 
 export default function ResultPage({ route, navigation }) {
   //receives picture from camera.page
   const { picture } = route.params;
   const { expectedString } = route.params;
 
-  //send to back end
-  //const res = await axios.post('enter-backend-url',{picture, expectedString});
+  const [score, setScore] = useState(0);
+  const [feedback, setFeedback] = useState('');
+
+  useEffect(() => {
+    getFeedback();
+  }, [])
+
+  const getFeedback = async () => {
+    //send image to back end and receive feedback
+
+    // formData.append('file', { url: picture.uri.replace('file://', ''), name: 'image.jpg', type: 'image/jpeg' })
+
+    // console.log(formData);
+
+    const image = Asset.fromURI(picture.uri);
+    await image.downloadAsync();
+    const compImage = await ImageManipulator.manipulateAsync(image.localUri || image.uri, [{resize:{width:100}}], {compress:0.2})
+
+    var formdata = new FormData();
+    formdata.append('file', compImage);
+
+
+    var requestOptions = {
+      method: 'POST',
+      body: formdata,
+      redirect: 'follow'
+    };
+
+    fetch("http://91e523a5.ngrok.io/ocr2?expect=test", requestOptions)
+      .then(response => response.text())
+      .then(result => console.log(result))
+      .catch(error => console.log('error', error));
+  }
+
 
   //display image, display result, display grade
   return (
     <View style={styles.container}>
       <View style={styles.photo}>
         <Image
-            style={{ width: 200, height: 300 }}
-            source={{ uri: picture.uri }}
-          />
+          style={{ width: 200, height: 300 }}
+          source={{ uri: picture.uri }}
+        />
       </View>
       <View style={styles.result}>
-          <Text style={styles.resulttext}>This would be the result</Text>
+        <Text style={styles.resulttext}>Result = {feedback}</Text>
       </View>
       <View style={styles.score}>
-          <Text>This would say the score</Text>
+        <Text>Score = {score}</Text>
       </View>
     </View>
   )
