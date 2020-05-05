94d48afe0a294905d17032adab54d40fd4de6c67
Mie-zi <60592802+Mie-zi@users.noreply.github.com>
2020-05-05 16:46:36 -0500

    Add files via upload

--- complete/src/main/java/com/example/restservice/VisionController.java
+++ complete/src/main/java/com/example/restservice/VisionController.java
@@ -1,63 +1,52 @@
 package com.example.restservice;
 
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.vision.v1.AnnotateImageResponse;
-import com.google.cloud.vision.v1.EntityAnnotation;
-import com.google.cloud.vision.v1.Feature.Type;
-
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.util.LinkedHashMap;
-import java.util.Map;
-import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.gcp.vision.CloudVisionTemplate;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
-import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.ModelAndView;
 
-/**
- * Code sample demonstrating Cloud Vision usage within the context of Spring Framework using Spring
- * Cloud GCP libraries. The sample is written as a Spring Boot application to demonstrate a
- * practical application of this usage.
- */
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
+
+
+
 @RestController
 public class VisionController {
 
+	/**ResourceLoader used to get image from url*/
 	@Autowired private ResourceLoader resourceLoader;
 
-	// [START spring_vision_autowire]
+	/**google vision api*/
 	@Autowired private CloudVisionTemplate cloudVisionTemplate;
-	// [END spring_vision_autowire]
 
-	@GetMapping("/getText")
-	public String getText(@RequestParam(value = "url", defaultValue = "abc123") String url) {
-//		 [START spring_vision_text_extraction]
+
+	/**
+	 * Receive get request from the client, send back a json
+	 * @param url url of the image
+	 * @param expect expected string
+	 * @return the json contains ocr result and similarity
+	 */
+	@GetMapping("/ocr")
+	public OcrResult ocr(@RequestParam(value = "url") String url, @RequestParam(value = "expect") String expect) {
+		System.out.println("Request received " + DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss").format(LocalDateTime.now()));
 		Resource image = this.resourceLoader.getResource(url);
 		String textFromImage = this.cloudVisionTemplate.extractTextFromImage(image);
-		return "Text from image: " + textFromImage;
-//		 [END spring_vision_text_extraction]
-		//return url;
+		System.out.println("ocr_result: " + textFromImage);
+		System.out.println("expect: " + expect);
+		System.out.println("url: " + url);
+		return new OcrResult(textFromImage, compareString(textFromImage, expect));
 	}
 
-	@GetMapping("/compareText")
-	public String compareText(@RequestParam(value = "url") String url, @RequestParam(value = "expect") String expect) {
-		Resource image = this.resourceLoader.getResource(url);
-		String textFromImage = this.cloudVisionTemplate.extractTextFromImage(image);
-		return compareString(textFromImage, expect);
-	}
-
-	@GetMapping("/test")
-	public String test(@RequestParam(value = "first") String first, @RequestParam(value = "second") String second) {
-		return compareString(first, second);
-	}
-
-	private String compareString(String first, String second) {
+	/**
+	 * get similarity of two strings
+	 * @param first first string
+	 * @param second second string
+	 * @return the similarity of two strings
+	 */
+	private double compareString(String first, String second) {
 		first = first.trim();
 		second = second.trim();
 		int len1 = first.length();
@@ -81,8 +70,7 @@ public class VisionController {
 						dif[i - 1][j] + 1);
 			}
 		}
-		double similarity =1 - (double) dif[len1][len2] / Math.max(first.length(), second.length());
-		return "Similarity of \""+first+"\" and \""+second+"\": " + similarity * 100 + "%";
+		return  1 - (double) dif[len1][len2] / Math.max(first.length(), second.length());
 	}
 
 	private static int min(int... is) {
